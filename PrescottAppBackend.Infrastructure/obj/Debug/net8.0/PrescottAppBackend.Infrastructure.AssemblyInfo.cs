5e06ecca8c51c40f6ee352832ef4d1199ae7887b
Suffi-Ullah <74228315+Suffi-Ullah@users.noreply.github.com>
2024-10-02 13:24:15 +0400

    git ignore file

--- PrescottAppBackend.Infrastructure/obj/Debug/net8.0/PrescottAppBackend.Infrastructure.AssemblyInfo.cs
+++ PrescottAppBackend.Infrastructure/obj/Debug/net8.0/PrescottAppBackend.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrescottAppBackend.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fd1dec92f596b1059410cf3f95946ca3132d517")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrescottAppBackend.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrescottAppBackend.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
