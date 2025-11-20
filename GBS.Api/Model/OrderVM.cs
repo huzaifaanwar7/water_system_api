6d7c51839f0d4383b5a8e53164bd2dbdd8535f55
huzaifaanwar7 <huzaifaanwar094@gmail.com>
2025-11-20 16:36:37 +0500

    Order VM

---
+++ GBS.Api/Model/OrderVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GBS.Api.Model
+{
+    public class OrderVM
+    {
+        public int Id { get; set; }
+        public string OrderNumber { get; set; }
+        public int ClientIdFk { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public int StatusIdFk { get; set; }
+        public string Status { get; set; }
+        public decimal TotalQuantity { get; set; }
+        public decimal TotalAmount { get; set; }
+        public decimal AdvanceAmount { get; set; }
+        public decimal BalanceAmount { get; set; }
+        public string Notes { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
