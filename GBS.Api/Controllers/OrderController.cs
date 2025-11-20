ce582879372871fbb60a248e42818025190a11c0
huzaifaanwar7 <huzaifaanwar094@gmail.com>
2025-11-20 16:59:47 +0500

    delete unnecessary files

---
+++ GBS.Api/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using GBS.Api.Model;
+using GBS.Service;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace GBS.Api.Controller
+{
+
+}
