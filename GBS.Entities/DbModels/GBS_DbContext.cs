2e5e3ce38b76b2f41f0eff9df665d7a5cc79ca8e
huzaifaanwar7 <huzaifaanwar094@gmail.com>
2025-11-20 16:34:56 +0500

    ORder List VM

--- GBS.Entities/DbModels/GBS_DbContext.cs
+++ GBS.Entities/DbModels/GBS_DbContext.cs
@@ -61,7 +61,10 @@ public partial class GBS_DbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=MUHAMMAD-ZEE;Database=gbs_dev;Integrated Security=True;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer(
+    "Server=192.227.71.86,4431;Database=gbs_dev;User Id=ihs;Password=ihs@5678;TrustServerCertificate=True;"
+);
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
