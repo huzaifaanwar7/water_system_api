3f79d352c85709df52963a8804a28a6b135103e7
huzaifaanwar7 <huzaifaanwar094@gmail.com>
2025-11-20 19:07:05 +0500

    update

--- GBS.Api/Controllers/OrderController.cs
+++ GBS.Api/Controllers/OrderController.cs
@@ -1,5 +1,5 @@
-﻿using GBS.Api.Model;
-using GBS.Service;
+﻿using GBS.Data.Model;
+using GBS.Service.Service;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Linq;
@@ -8,5 +8,168 @@ using System.Threading.Tasks;
 
 namespace GBS.Api.Controller
 {
+    [Route("[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
 
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var orders = await _orderService.GetOrderList();
+                if (orders != null && orders.Any())
+                {
+                    var response = orders.Select(order => new OrderVM
+                    {
+                        Id = order.Id,
+                        OrderNumber = order.OrderNumber,
+                        ClientIdFk = order.ClientIdFk,
+                        OrderDate = order.OrderDate,
+                        DeliveryDate = order.DeliveryDate,
+                        StatusIdFk = order.StatusIdFk,
+                        Status = order.StatusIdFkNavigation?.Name,
+                        TotalQuantity = order.TotalQuantity,
+                        TotalAmount = order.TotalAmount,
+                        AdvanceAmount = order.AdvanceAmount,
+                        BalanceAmount = order.BalanceAmount,
+                        Notes = order.Notes,
+                        CreatedBy = "Hard Coded Name",
+                        CreatedDate = order.CreatedDate,
+                        ModifiedDate = order.ModifiedDate,
+                        ModifiedBy = "Hard Coded Name"
+                    }).ToList();
+
+                    return Ok(new BaseResponse
+                    {
+                        status = HttpStatusCode.OK,
+                        data = response
+                    });
+                }
+
+                return NotFound(new BaseResponse
+                {
+                    status = HttpStatusCode.NotFound,
+                    message = "No orders found."
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new BaseResponse
+                {
+                    status = HttpStatusCode.InternalServerError,
+                    message = $"An error occurred: {ex.Message}"
+                });
+            }
+        }
+
+        [HttpGet("{OrderId}")]
+        public async Task<IActionResult> GetDetails([FromRoute] int OrderId)
+        {
+            try
+            {
+                var order = await _orderService.GetOrderById(OrderId);
+                if (order != null)
+                {
+                    var response = new OrderVM
+                    {
+                        Id = order.Id,
+                        OrderNumber = order.OrderNumber,
+                        ClientIdFk = order.ClientIdFk,
+                        OrderDate = order.OrderDate,
+                        DeliveryDate = order.DeliveryDate,
+                        StatusIdFk = order.StatusIdFk,
+                        Status = order.StatusIdFkNavigation?.Name,
+                        TotalQuantity = order.TotalQuantity,
+                        TotalAmount = order.TotalAmount,
+                        AdvanceAmount = order.AdvanceAmount,
+                        BalanceAmount = order.BalanceAmount,
+                        Notes = order.Notes,
+                        CreatedBy = "Hard Coded Name",
+                        CreatedDate = order.CreatedDate,
+                        ModifiedDate = order.ModifiedDate,
+                        ModifiedBy = "Hard Coded Name"
+                    };
+
+                    return Ok(new BaseResponse
+                    {
+                        status = HttpStatusCode.OK,
+                        data = response
+                    });
+                }
+
+                return NotFound(new BaseResponse
+                {
+                    status = HttpStatusCode.NotFound,
+                    message = "No order found."
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new BaseResponse
+                {
+                    status = HttpStatusCode.InternalServerError,
+                    message = $"An error occurred: {ex.Message}"
+                });
+            }
+        }
+
+        [HttpGet("Client/{ClientId}")]
+        public async Task<IActionResult> GetOrdersByClient([FromRoute] int ClientId)
+        {
+            try
+            {
+                var orders = await _orderService.GetOrdersByClientId(ClientId);
+                if (orders != null && orders.Any())
+                {
+                    var response = orders.Select(order => new OrderVM
+                    {
+                        Id = order.Id,
+                        OrderNumber = order.OrderNumber,
+                        ClientIdFk = order.ClientIdFk,
+                        OrderDate = order.OrderDate,
+                        DeliveryDate = order.DeliveryDate,
+                        StatusIdFk = order.StatusIdFk,
+                        Status = order.StatusIdFkNavigation?.Name,
+                        TotalQuantity = order.TotalQuantity,
+                        TotalAmount = order.TotalAmount,
+                        AdvanceAmount = order.AdvanceAmount,
+                        BalanceAmount = order.BalanceAmount,
+                        Notes = order.Notes,
+                        CreatedBy = "Hard Coded Name",
+                        CreatedDate = order.CreatedDate,
+                        ModifiedDate = order.ModifiedDate,
+                        ModifiedBy = "Hard Coded Name"
+                    }).ToList();
+
+                    return Ok(new BaseResponse
+                    {
+                        status = HttpStatusCode.OK,
+                        data = response
+                    });
+                }
+
+                return NotFound(new BaseResponse
+                {
+                    status = HttpStatusCode.NotFound,
+                    message = "No orders found for this client."
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new BaseResponse
+                {
+                    status = HttpStatusCode.InternalServerError,
+                    message = $"An error occurred: {ex.Message}"
+                });
+            }
+        }
+    }
 }
---
+++ GBS.Service/Service/OrderService.cs
@@ -0,0 +1,53 @@
+﻿using GBS.Entities.DbModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GBS.Service.Service
+{
+    public interface IOrderService
+    {
+        Task<List<Order>> GetOrderList();
+        Task<Order> GetOrderById(int id);
+        Task<List<Order>> GetOrdersByClientId(int clientId);
+    }
+
+    public class OrderService : IOrderService
+    {
+        private readonly GBS_DbContext _dbContext;
+
+        public OrderService(GBS_DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Order>> GetOrderList()
+        {
+            var orders = await _dbContext.Orders
+               .Include(o => o.ClientIdFkNavigation)
+               .Include(o => o.StatusIdFkNavigation)
+               .ToListAsync();
+            return orders;
+        }
+
+        public async Task<Order> GetOrderById(int id)
+        {
+            return await _dbContext.Orders
+                .Where(o => o.Id == id)
+                .Include(o => o.ClientIdFkNavigation)
+                .Include(o => o.StatusIdFkNavigation)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Order>> GetOrdersByClientId(int clientId)
+        {
+            return await _dbContext.Orders
+                .Where(o => o.ClientIdFk == clientId)
+                .Include(o => o.ClientIdFkNavigation)
+                .Include(o => o.StatusIdFkNavigation)
+                .ToListAsync();
+        }
+    }
+}
