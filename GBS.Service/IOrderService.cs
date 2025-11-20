822533e88e24db8b0dc0da44ba95142eefe87781
huzaifaanwar7 <huzaifaanwar094@gmail.com>
2025-11-20 16:44:16 +0500

    Order Service

---
+++ GBS.Service/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using GBS.Entities.DbModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GBS.Service
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<Order>> GetOrderList();
+        Task<Order> GetOrderById(int orderId);
+        Task<IEnumerable<Order>> GetOrdersByClientId(int clientId);
+        Task<string> GetUserNameById(int userId);
+    }
+}
