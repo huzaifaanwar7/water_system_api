70fbffd2f7e7207db54fd7eee6ae78e4b7012ee8
huzaifaanwar7 <huzaifaanwar094@gmail.com>
2025-11-27 18:15:38 +0500

    Order Cost Vm created

---
+++ GBS.Api/Model/OrderCostVM.cs
@@ -0,0 +1,18 @@
+using System;
+namespace GBS.Api.Model
+{
+    public class OrderCostVM
+    {
+        public int Id { get; set; }
+        public int OrderIdFk { get; set; }
+        public int CostCategoryIdFk { get; set; }
+        public string CostDescription { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal UnitCost { get; set; }
+        public decimal TotalCost { get; set; }
+        public DateTime CostDate { get; set; }
+        public string VendorName { get; set; }
+        public string InvoiceNumber { get; set; }
+        public string Notes { get; set; }
+    }
+}
