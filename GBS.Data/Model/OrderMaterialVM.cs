299364ddd487db7afba1b5253415e96d96383c6e
huzaifaanwar7 <huzaifaanwar094@gmail.com>
2025-11-27 21:46:36 +0500

    Material Page

---
+++ GBS.Data/Model/OrderMaterialVM.cs
@@ -0,0 +1,16 @@
+﻿namespace GBS.Data.Model
+{
+    public class OrderMaterialVM
+    {
+        public int Id { get; set; }
+        public int? OrderIdFk { get; set; }
+        public int? MaterialIdFk { get; set; }
+        public decimal? QuantityUsed { get; set; }
+        public decimal? UnitCost { get; set; }
+        public decimal? TotalCost { get; set; }
+        public DateTime? UsageDate { get; set; }
+        public string? Notes { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+    }
+}
