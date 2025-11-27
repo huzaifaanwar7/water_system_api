ed9415bbb3b77c233953006335b23d24ecccaf46
huzaifaanwar7 <huzaifaanwar094@gmail.com>
2025-11-27 13:59:41 +0500

    Order List API

--- GBS.Api/Controllers/OrderController.cs
+++ GBS.Api/Controllers/OrderController.cs
@@ -1,9 +1,11 @@
 ﻿using GBS.Data.Model;
 using GBS.Service;
 using GBS.Service.Service;  // Use GBS.Service.Service namespace
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -70,6 +72,57 @@ namespace GBS.Api.Controller
                 });
             }
         }
+        [HttpGet("GetOrderList")]
+        public async Task<IActionResult> GetOrderList()
+        {
+            try
+            {
+                var orders = await _orderService.GetOrderList();
+                if (orders != null && orders.Any())
+                {
+                    var response = orders.Select(order => new OrderVM
+                    {
+                        Id = order.Id,
+                        OrderNumber = order.OrderNumber,
+                        ClientIdFk = order.ClientIdFk,
+                        OrderDate = order.OrderDate,
+                        DeliveryDate = order.DeliveryDate,
+                        StatusIdFk = order.StatusIdFk,
+                        Status = order.StatusIdFkNavigation?.Name,
+                        TotalQuantity = order.TotalQuantity,
+                        TotalAmount = order.TotalAmount,
+                        AdvanceAmount = order.AdvanceAmount,
+                        BalanceAmount = order.BalanceAmount,
+                        Notes = order.Notes,
+                        CreatedBy = "Hard Coded Name",
+                        CreatedDate = order.CreatedDate,
+                        ModifiedDate = order.ModifiedDate,
+                        ModifiedBy = "Hard Coded Name"
+                    }).ToList();
+
+                    return Ok(new
+                    {
+                        status = System.Net.HttpStatusCode.OK,
+                        data = response
+                    });
+                }
+
+                return NotFound(new
+                {
+                    status = System.Net.HttpStatusCode.NotFound,
+                    message = "No Orders Found"
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    status = System.Net.HttpStatusCode.InternalServerError,
+                    message = $"An Error Occurred: {ex.Message}"
+                });
+            }
+        }
+
 
         [HttpGet("{OrderId}")]
         public async Task<IActionResult> GetDetails([FromRoute] int OrderId)
