1b4dfe8e5c6f579e92fe459d0553162ebeab09c5
huzaifaanwar7 <huzaifaanwar094@gmail.com>
2025-11-27 21:24:22 +0500

    Order Item VM created

---
+++ GBS.Data/Model/OrderItemVM.cs
@@ -0,0 +1,17 @@
+﻿namespace GBS.Data.Model
+{
+    public class OrderItemVM
+    {
+        public int Id { get; set; }
+        public int? OrderIdFk { get; set; }
+        public int? ProductIdFk { get; set; }
+        public decimal? Quantity { get; set; }
+        public int? SizeIdFk { get; set; }
+        public string? Color { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public decimal? TotalPrice { get; set; }
+        public string? SpecialInstructions { get; set; }
+        public bool? IsCompleted { get; set; }
+        public decimal? CompletedQuantity { get; set; }
+    }
+}
