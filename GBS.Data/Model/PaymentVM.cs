6f799a33f60eb15bd0bd8de887b1d09d1dc589c4
huzaifaanwar7 <huzaifaanwar094@gmail.com>
2025-12-02 13:22:08 +0500

    Errors fixed

--- GBS.Data/Model/PaymentVM.cs
+++ GBS.Data/Model/PaymentVM.cs
@@ -8,21 +8,21 @@ namespace GBS.Data.Model
 {
     public partial class PaymentVM
     {
-        public int Id { get; set; }
-        public int OrderIdFk { get; set; }
+        public int? Id { get; set; }
+        public int? OrderIdFk { get; set; }
 
-        public DateTime PaymentDate { get; set; }
+        public DateTime? PaymentDate { get; set; }
 
-        public decimal Amount { get; set; }
-        public int PaymentMethodIdFk { get; set; }
+        public decimal? Amount { get; set; }
+        public int? PaymentMethodIdFk { get; set; }
 
         public string? ReferenceNumber { get; set; }
 
         public string? Notes { get; set; }
 
-        public string? CreatedBy { get; set; }
+        public int? CreatedBy { get; set; }
 
-        public DateTime CreatedDate { get; set; }
+        public DateTime? CreatedDate { get; set; }
 
 
     }
