c98939b93f8579bddd15917a02d473ae8fee3c88
huzaifaanwar7 <huzaifaanwar094@gmail.com>
2025-12-02 13:11:09 +0500

    PaymentVM Created

---
+++ GBS.Data/Model/PaymentVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBS.Data.Model
+{
+    public partial class PaymentVM
+    {
+        public int Id { get; set; }
+        public int OrderIdFk { get; set; }
+
+        public DateTime PaymentDate { get; set; }
+
+        public decimal Amount { get; set; }
+        public int PaymentMethodIdFk { get; set; }
+
+        public string? ReferenceNumber { get; set; }
+
+        public string? Notes { get; set; }
+
+        public string? CreatedBy { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+
+    }
+}
